5449dec8327f66b77c46f9da8efbdd454f78ec55
Nick Ormsby <nickormsby@comcast.net>
2016-03-19 02:21:17 -0400

    Updated stored procedures

---
+++ Vote.Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vote.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> SelectAll();
+        T SelectByID(object id);
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(object id);
+        void Save();
+    }
+}
